9c0dbaae5085f738865834d198b7e0f4fc5e9a6a
Leonardo Silva <leonardohsilva.dev@gmail.com>
2022-07-18 21:19:50 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    // '!<rootDir>/src/main/**/*',
+    // '!<rootDir>/src/**/index.ts',
+    // '!**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+  // setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
+  // testPathIgnorePatterns: [
+  //   '<rootDir>/node_modules/',
+  //   '<rootDir>/tests/e2e/cypress'
+  // ],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  // moduleNameMapper: {
+  //   '@/tests/(.*)': '<rootDir>/tests/$1',
+  //   '@/(.*)': '<rootDir>/src/$1',
+  //   '\\.scss$': 'identity-obj-proxy'
+  // }
+}
