cc93e36ac629804a57faace9339b066dd5547106
Leonardo Silva <leonardohsilva.dev@gmail.com>
2022-07-30 16:57:37 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     '<rootDir>/src/**/*.{ts,tsx}',
     // '!<rootDir>/src/main/**/*',
     // '!<rootDir>/src/**/index.ts',
-    // '!**/*.d.ts'
+    '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
   // setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
@@ -20,6 +20,6 @@ module.exports = {
   moduleNameMapper: {
     // '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1',
-    // '\\.scss$': 'identity-obj-proxy'
+    '\\.scss$s': 'identity-obj-proxy'
   }
 }
