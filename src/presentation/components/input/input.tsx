5190191645cd9ac59d8e4ced90c13af3eafd829a
Leonardo Silva <leonardohsilva.dev@gmail.com>
2022-10-10 19:16:20 -0300

    feat: ensure Login present valid state if email and password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -13,11 +13,11 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
 
   const getTitle = (): string => {
-    return error
+    return error ?? 'Ok'
   }
 
   return (
