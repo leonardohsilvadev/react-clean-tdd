4e5a4dd19e7a84f1522dad6c22ce15a2c2311a64
Leonardo Silva <leonardohsilva.dev@gmail.com>
2022-11-24 21:37:50 -0300

    test: ensure ValidationBuilder returns a list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,19 +1,35 @@
-import { EmailValidation, MinLengthValidation, RequiredFieldValidation } from "@/validation/validators"
+import {
+  EmailValidation,
+  MinLengthValidation,
+  RequiredFieldValidation
+} from "@/validation/validators"
 import { ValidationBuilder as sut } from "./validation-builder"
+import { faker } from '@faker-js/faker'
 
 describe('ValidationBuilder', () => {
+  const field = faker.database.column()
+  const length = Number(faker.random.numeric())
   test('Should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').required().build()
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    const validations = sut.field(field).required().build()
+    expect(validations).toEqual([new RequiredFieldValidation(field)])
   })
 
   test('Should return EmailValidation', () => {
-    const validations = sut.field('any_field').email().build()
-    expect(validations).toEqual([new EmailValidation('any_field')])
+    const validations = sut.field(field).email().build()
+    expect(validations).toEqual([new EmailValidation(field)])
   })
 
   test('Should return MinLengthValidation', () => {
-    const validations = sut.field('any_field').min(5).build()
-    expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
+    const validations = sut.field(field).min(length).build()
+    expect(validations).toEqual([new MinLengthValidation(field, length)])
+  })
+
+  test('Should return a list of validations', () => {
+    const validations = sut.field(field).required().email().min(length).build()
+    expect(validations).toEqual([
+      new RequiredFieldValidation(field),
+      new EmailValidation(field),
+      new MinLengthValidation(field, length)
+    ])
   })
 })
