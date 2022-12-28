1d43ba49f60a985e567c518bae801120afeca910
Leonardo Silva <leonardohsilva.dev@gmail.com>
2022-12-28 11:48:17 -0300

    test: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,13 @@
+import { ValidationComposite } from "@/validation/validators"
+import { ValidationBuilder } from "@/validation/validators/builder/validation-builder"
+import { makeSignUpValidation } from "./signup-validation-factory"
+
+describe('SignUpValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      ...ValidationBuilder.field('email').required().email().build(),
+      ...ValidationBuilder.field('password').required().min(5).build()
+    ]))
+  })
+})
