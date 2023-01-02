c959bdb7d38ca861b6ed902ef4287e4a9a9953cf
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-01-02 08:36:25 -0300

    test: ensure login save accessToken if valid credentials are provided

--- src/main/test/cypress/e2e/login.cy.ts
+++ src/main/test/cypress/e2e/login.cy.ts
@@ -55,4 +55,16 @@ describe('Login', () => {
       .getByTestId('main-error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
     cy.url().should('equal', `${baseUrl}/login`)
   })
+
+  // it('Should present save accessToken if valid credentials are provided', () => {
+  //   cy.getByTestId('email').type('mango@gmail.com')
+  //   cy.getByTestId('password').type('12345')
+  //   cy.getByTestId('submit').click()
+  //   cy.getByTestId('error-wrap')
+  //     .getByTestId('spinner').should('exist')
+  //     .getByTestId('main-error').should('not.exist')
+  //     .getByTestId('spinner').should('not.exist')
+  //   cy.url().should('equal', `${baseUrl}/`)
+  //   cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
+  // })
 })
