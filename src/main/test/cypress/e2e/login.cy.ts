27f7c932a406acc38589736daf71cf8688c31fd5
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-01-03 08:06:12 -0300

    refactor: change test to simulate submit with enter key

--- src/main/test/cypress/e2e/login.cy.ts
+++ src/main/test/cypress/e2e/login.cy.ts
@@ -76,8 +76,7 @@ describe('Login', () => {
       body: { invalidProperty: faker.datatype.uuid() }
     })
     cy.getByTestId('email').type(faker.internet.email())
-    cy.getByTestId('password').type(faker.random.alphaNumeric(5))
-    cy.getByTestId('submit').click()
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5)).type('{enter}')
     cy.getByTestId('spinner').should('not.exist')
     cy.getByTestId('main-error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
     cy.url().should('equal', `${baseUrl}/login`)
