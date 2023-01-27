6d0306a0b6d6ea2fa0518a35074c79ff4fac5604
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-01-26 21:58:05 -0300

    feat: ensure AxiosHttpClient returns correct value on faliure

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -18,7 +18,12 @@ export class AxiosHttpClient implements HttpPostClient, HttpGetClient {
   }
 
   async get (params: HttpGetParams): Promise<HttpResponse> {
-    const axiosResponse = await axios.get(params.url)
+    let axiosResponse
+    try {
+      axiosResponse = await axios.get(params.url)
+    } catch (error) {
+      axiosResponse = error.response
+    }
     return {
       statusCode: axiosResponse.status,
       body: axiosResponse.data
